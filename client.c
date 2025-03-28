0bfc653f8f8ab69ccf4ec2f7bfe231d95c2a0bf8
Dashulka1 <144533783+Dashulka1@users.noreply.github.com>
2025-03-28 08:54:49 +0300

    Add files via upload

--- client.c
+++ client.c
@@ -1,99 +1,128 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <arpa/inet.h>
-#include <sys/socket.h>
 #include <unistd.h>
+#include <arpa/inet.h>
+#include <time.h>
 
 #define BUFFER_SIZE 1024
 
-void interactive_mode(int sockfd) {
-    char buffer[BUFFER_SIZE];
-    int guess;
+void interactive_mode(int sock);
+void automatic_mode(int sock);
 
+int main(int argc, char *argv[]) {
+    if (argc < 3 || argc > 4) {
+        fprintf(stderr, "Использование: %s <адрес сервера> <порт> [-a]\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+    
+    int sock;
+    struct sockaddr_in server_addr;
+    int port = atoi(argv[2]);
+    int auto_mode = (argc == 4 && strcmp(argv[3], "-a") == 0);
+
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        perror("Ошибка создания сокета");
+        exit(EXIT_FAILURE);
+    }
+
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(port);
+
+    if (inet_pton(AF_INET, argv[1], &server_addr.sin_addr) <= 0) {
+        perror("Ошибка в адресе сервера");
+        close(sock);
+        exit(EXIT_FAILURE);
+    }
+
+    if (connect(sock, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
+        perror("Ошибка подключения");
+        close(sock);
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Клиент подключен к серверу %s:%d\n", argv[1], port);
+
+    if (auto_mode) {
+        printf("Запущен автоматический режим.\n");
+        automatic_mode(sock);
+    } else {
+        printf("Запущен интерактивный режим.\n");
+        interactive_mode(sock);
+    }
+
+    close(sock);
+    return 0;
+}
+
+// Интерактивный режим (игрок - человек)
+void interactive_mode(int sock) {
+    char buffer[BUFFER_SIZE];
     while (1) {
         printf("Введите число: ");
-        scanf("%d", &guess);
-        sprintf(buffer, "%d", guess);
-        send(sockfd, buffer, strlen(buffer), 0);
+        if (fgets(buffer, BUFFER_SIZE, stdin) == NULL) {
+            printf("Ошибка ввода.\n");
+            break;
+        }
+        
+        buffer[strcspn(buffer, "\n")] = '\0';  // убираем символ новой строки
+        
+        if (send(sock, buffer, strlen(buffer), 0) < 0) {
+            perror("Ошибка отправки данных");
+            break;
+        }
 
         memset(buffer, 0, BUFFER_SIZE);
-        recv(sockfd, buffer, BUFFER_SIZE, 0);
+        int n = recv(sock, buffer, BUFFER_SIZE - 1, 0);
+        if (n <= 0) {
+            printf("Сервер закрыл соединение.\n");
+            break;
+        }
+        buffer[n] = '\0';
+        printf("Ответ сервера: %s\n", buffer);
 
-        printf("Сервер: %s\n", buffer);
-
-        if (strcmp(buffer, "win") == 0) {
+        if (strcmp(buffer, "=") == 0) {
             printf("Вы угадали число!\n");
             break;
         }
     }
 }
 
-void auto_mode(int sockfd) {
+// Автоматический режим (игрок - машина)
+void automatic_mode(int sock) {
     char buffer[BUFFER_SIZE];
     int low = 1, high = 100, guess;
 
+    srand(time(NULL));
+
     while (1) {
-        guess = (low + high) / 2;
-        printf("Авто-игрок отправляет: %d\n", guess);
-        sprintf(buffer, "%d", guess);
-        send(sockfd, buffer, strlen(buffer), 0);
+        guess = (low + high) / 2; // Бинарный поиск
+        printf("Клиент предполагает число: %d\n", guess);
+        snprintf(buffer, BUFFER_SIZE, "%d", guess);
+
+        if (send(sock, buffer, strlen(buffer), 0) < 0) {
+            perror("Ошибка отправки данных");
+            break;
+        }
 
         memset(buffer, 0, BUFFER_SIZE);
-        recv(sockfd, buffer, BUFFER_SIZE, 0);
-
-        printf("Сервер ответил: %s\n", buffer);
-
-        if (strcmp(buffer, "win") == 0) {
-            printf("Авто-игрок угадал число!\n");
+        int n = recv(sock, buffer, BUFFER_SIZE - 1, 0);
+        if (n <= 0) {
+            printf("Сервер закрыл соединение.\n");
             break;
-        } else if (strcmp(buffer, "more") == 0) {
-            low = guess + 1;
-        } else if (strcmp(buffer, "less") == 0) {
-            high = guess - 1;
         }
+        buffer[n] = '\0';
+        printf("Ответ сервера: %s\n", buffer);
+
+        if (strcmp(buffer, "=") == 0) {
+            printf("Клиент угадал число: %d\n", guess);
+            break;
+        } else if (strcmp(buffer, "<") == 0) {
+            high = guess - 1;
+        } else if (strcmp(buffer, ">") == 0) {
+            low = guess + 1;
+        }
+
+        sleep(1); // для наглядности. медленнее будет перебор
     }
-}
-
-int main(int argc, char *argv[]) {
-    if (argc != 3) {
-        fprintf(stderr, "Использование: %s <IP> <режим>\n", argv[0]);
-        return 1;
-    }
-
-    int sockfd;
-    struct sockaddr_in server_addr;
-    char *mode = argv[2];
-
-    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
-        perror("Ошибка при создании сокета");
-        return 1;
-    }
-
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(12345);
-    if (inet_pton(AF_INET, argv[1], &server_addr.sin_addr) <= 0) {
-        perror("Ошибка преобразования адреса");
-        return 1;
-    }
-
-    if (connect(sockfd, (struct sockaddr*)&server_addr, sizeof(server_addr)) == -1) {
-        perror("Ошибка подключения");
-        return 1;
-    }
-
-    printf("Подключено к серверу %s\n", argv[1]);
-
-    if (strcmp(mode, "interactive") == 0) {
-        interactive_mode(sockfd);
-    } else if (strcmp(mode, "auto") == 0) {
-        auto_mode(sockfd);
-    } else {
-        fprintf(stderr, "Ошибка: неизвестный режим %s\n", mode);
-        return 1;
-    }
-
-    close(sockfd);
-    printf("Отключено от сервера\n");
-    return 0;
 }
--- server.c
+++ server.c
@@ -1,96 +1,105 @@
-#include <stdio.h> //для ввода/вывода (например, printf).
-#include <stdlib.h> // для стандартных функций (например, atoi для преобразования строки в число).
-#include <string.h> //для работы со строками (например, memset для обнуления массива).
-#include <arpa/inet.h> // для работы с интернет-адресами (например, для преобразования IP-адреса в строку с помощью
-#include <sys/socket.h> // для работы с сокетами (например, socket, bind, listen).
-#include <unistd.h> //  для системных вызовов (например, close для закрытия сокета).
-#include <time.h> //для работы с временем (например, генерация случайных чисел на основе времени с помощью rand).
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <time.h>
 
 #define BUFFER_SIZE 1024
+
+// Функция для обработки одного клиента
+void handle_client(int client_sock, struct sockaddr_in client_addr) {
+    char buffer[BUFFER_SIZE];
+    int n;
+    int target = rand() % 100 + 1; // Случайное число от 1 до 100
+    char client_ip[INET_ADDRSTRLEN];
+    inet_ntop(AF_INET, &client_addr.sin_addr, client_ip, INET_ADDRSTRLEN);
+    
+    printf("%s: Клиент подключился.\n", client_ip);
+
+    while (1) {
+        memset(buffer, 0, BUFFER_SIZE);
+        n = recv(client_sock, buffer, BUFFER_SIZE - 1, 0);
+        if (n <= 0) {
+            break;
+        }
+        buffer[n] = '\0';
+        printf("%s: Получено сообщение: %s\n", client_ip, buffer);
+        
+        int guess = atoi(buffer);
+        char response[BUFFER_SIZE];
+        if (guess < target) {
+            strcpy(response, ">");
+        } else if (guess > target) {
+            strcpy(response, "<");
+        } else {
+            strcpy(response, "=");
+        }
+        
+        send(client_sock, response, strlen(response), 0);
+        printf("%s: Отправлен ответ: %s\n", client_ip, response);
+        
+        if (guess == target) {
+            break;
+        }
+    }
+    
+    printf("%s: Клиент отключился.\n", client_ip);
+    close(client_sock);
+}
 
 int main(int argc, char *argv[]) {
     if (argc != 2) {
         fprintf(stderr, "Использование: %s <порт>\n", argv[0]);
-        return 1;
+        exit(EXIT_FAILURE);
     }
-
-    int port = atoi(argv[1]);
-    if (port <= 0 || port > 65535) {
-        fprintf(stderr, "Ошибка: некорректный порт\n");
-        return 1; 
-    }
-
-    int server_fd, client_fd;
+    
+    int server_sock, client_sock;
     struct sockaddr_in server_addr, client_addr;
-    socklen_t client_len = sizeof(client_addr);
-    char buffer[BUFFER_SIZE];
-
+    socklen_t addr_len = sizeof(client_addr);
+    int port = atoi(argv[1]);
+    
     srand(time(NULL));
-
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == -1) { //AF_INET для IPV4
-        perror("Ошибка при создании сокета");
-        return 1;
+    
+    // Создание сокета
+    if ((server_sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        perror("Ошибка создания сокета");
+        exit(EXIT_FAILURE);
     }
-
+    
+    // Настройка адреса сервера
     server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(port);
     server_addr.sin_addr.s_addr = INADDR_ANY;
-
-    if (bind(server_fd, (struct sockaddr*)&server_addr, sizeof(server_addr)) == -1) {
-        perror("Ошибка при привязке сокета");
-        close(server_fd);
-        return 1;
+    server_addr.sin_port = htons(port);
+    
+    // Привязка сокета
+    if (bind(server_sock, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
+        perror("Ошибка bind");
+        close(server_sock);
+        exit(EXIT_FAILURE);
     }
-
-    if (listen(server_fd, 5) == -1) {
-        perror("Ошибка при прослушивании");
-        close(server_fd);
-        return 1;
+    
+    // Перевод сокета в режим прослушивания
+    if (listen(server_sock, 5) < 0) {
+        perror("Ошибка listen");
+        close(server_sock);
+        exit(EXIT_FAILURE);
     }
-
-    printf("Сервер запущен на порту %d...\n", port);
-
+    
+    printf("Сервер запущен на порту %d\n", port);
+    
+    // Основной цикл для приема соединений
     while (1) {
-        client_fd = accept(server_fd, (struct sockaddr*)&client_addr, &client_len);
-        if (client_fd == -1) {
-            perror("Ошибка при принятии подключения");
+        if ((client_sock = accept(server_sock, (struct sockaddr*)&client_addr, &addr_len)) < 0) {
+            perror("Ошибка accept");
             continue;
         }
-
-        printf("Подключение от %s:%d\n", inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
-
-        int secret_number = rand() % 100 + 1;
-        int attempts = 0;
-        int guess;
-
-        while (1) {
-            memset(buffer, 0, BUFFER_SIZE);
-            int bytes_received = recv(client_fd, buffer, BUFFER_SIZE, 0);
-            if (bytes_received <= 0) {
-                printf("Отключение клиента %s:%d\n", inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
-                break;
-            }
-
-            guess = atoi(buffer);
-            attempts++;
-
-            printf("[%s:%d] Попытка %d: %d\n", inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port), attempts, guess);
-
-            if (guess < secret_number) {
-                send(client_fd, "more", 4, 0);
-            } else if (guess > secret_number) {
-                send(client_fd, "less", 4, 0);
-            } else {
-                send(client_fd, "win", 3, 0);
-                printf("[%s:%d] Угадано число %d за %d попыток\n", inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port), secret_number, attempts);
-                break;
-            }
-        }
-
-        close(client_fd);
-        printf("Отключение клиента %s:%d\n", inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
+        
+        // Обработка клиента (в данном примере в одном процессе; для масштабирования можно создавать потоки)
+        handle_client(client_sock, client_addr);
     }
-
-    close(server_fd);
+    
+    close(server_sock);
     return 0;
 }
